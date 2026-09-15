99143050018f62229b7a4cad30f1cb49181717cc
Hannah Patricia Samson <hbsamson@up.edu.ph>
2026-09-15 14:06:23 +0800

    feat: add generated spring setup

---
+++ src/main/java/com/svi/tictactoespringboot/TictactoeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.svi.tictactoespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TictactoeSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/svi/tictactoespringboot/TictactoeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svi.tictactoespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
