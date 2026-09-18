05557ac30e4c5db49d59394ac5393742b48e5f11
Hannah Patricia Samson <hbsamson@up.edu.ph>
2026-09-18 10:15:56 +0800

    feat: add web config for cors

---
+++ src/main/java/com/svi/tictactoespringboot/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.svi.tictactoespringboot.config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final String[] origins;
+    public WebConfig(@Value("${app.cors.allowed-origins}") String origins) {
+        this.origins = origins.split(",");
+    }
+
+    @Override 
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedOrigins(origins).allowedMethods("GET","POST").allowedHeaders("*");
+    }
+}
